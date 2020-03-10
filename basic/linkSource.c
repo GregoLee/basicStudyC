21be07de42c50505cd39abc8f7400bee276e83a2
이재영(Partner) <lmyctx.lee@partner.samsung.com>
2020-03-11 08:47:35 +0900

    first C study

---
+++ basic/linkSource.c
@@ -0,0 +1,12 @@
+//
+// Created by invin on 2020-03-11.
+//
+/*
+ * inner form in child component and outer form in parent component
+ * https://stackoverflow.com/questions/40172270/angular-2-form-containing-child-component
+ *
+ * nested forms
+ * https://davembush.github.io/attaching-an-angular-child-component-s-form-to-a-parent/
+ *
+ *
+ */
